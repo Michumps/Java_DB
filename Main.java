41df2f57f8cc2dadb8b68ae7f830fd45e13bb4b2
Michael Meyers <michaelmeyers409111@gmail.com>
2026-03-07 15:21:53 -0600

    Added better input handling

--- Main.java
+++ Main.java
@@ -4,12 +4,42 @@ public class Main {
 	public static void main(String args[]){
 
 		Scanner scanner = new Scanner(System.in);
+		LinkedList linkedList = new LinkedList();
+		StorageManager storageManager = new StorageManager();
 
-		System.out.println("Enter your name: ");
+		linkedList.initializeList(storageManager.readFromFile());
 
-		String myName = scanner.nextLine();
+		MAIN_LOOP:
+		while(true) {
 
-		System.out.println(myName);
+			String command = scanner.nextLine();
+			command = command.trim(); // Remove leading/trailing whitespace
+
+			String[] tokens = command.split(" ");
+
+			if (tokens[0] == null || tokens[0].isEmpty()) {
+				System.out.println("Error reading from STDIN please try again");
+				continue;
+			}
+
+			switch(tokens[0]) {
+				case "SET":
+					linkedList.setValue(tokens[1], tokens[2]);
+					storageManager.append("SET;" + tokens[1] + ';' + tokens[2]);
+					break;
+
+				case "GET":
+					System.out.println(linkedList.getValue(tokens[1]));
+					System.out.flush();
+					break;
+				case "EXIT":
+					break MAIN_LOOP; // labeled break to break while loop
+				
+				default:
+					System.out.println("Error reading command, Syntax: SET <key> <value>, GET <key>, EXIT");
+			}
+
+		}
 
 		scanner.close();
 
